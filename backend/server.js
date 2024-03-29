6d02e1b34fe27c188ad94b13e7d6c469a39d7819
mehioitani <muhieddineitani04@gmail.com>
2024-03-30 01:55:41 +0200

    testing

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: "https://techtrove-8.onrender.com",
+    origin: "https://techtrove-9.onrender.com",
 
     credentials: true,
   })
