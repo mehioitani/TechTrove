14eef0158d4ad04487addcc10566109c77155672
mehioitani <muhieddineitani04@gmail.com>
2024-03-30 02:16:02 +0200

    deployment done successfully

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: "https://techtrove-9.onrender.com",
+    origin: "https://techtrove-ijiz.onrender.com",
 
     credentials: true,
   })
